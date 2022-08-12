550efd8175a3803d6dae65048cad412df22d9d76
Victor Su <victor.yuchen.su@gmail.com>
2022-08-12 11:27:03 -0400

    implemented changing book's read property through clicking icon

--- scripts.js
+++ scripts.js
@@ -78,17 +78,23 @@ function displayBooks() {
         card.append(addPropertyToCard(book.pages, 'Pages: '));
         card.append(addPropertyToCard(book.read, 'Read? '))
         //adds trash icon to card
-        let trashIcon = document.createElement('input');
-        trashIcon.setAttribute('type', 'image')
-        
-        trashIcon.setAttribute('class', '.card input:last-child')
-        trashIcon.setAttribute('src', 'media/delete.svg')
-        trashIcon.setAttribute('alt', 'trash card')
+        let trashIcon = createImageElement('media/delete.svg', 'trash icon');
+        trashIcon.setAttribute('class', 'icon trash-icon');
         trashIcon.addEventListener('click', function(e){
             removeCardFromLibrary(i);
             displayBooks();
         });
         card.append(trashIcon);
+
+        //adds book icon to card
+        let bookIcon = createImageElement('media/book.svg', 'book icon');
+        bookIcon.setAttribute('class', 'icon book-icon');
+        bookIcon.addEventListener('click', function(e){
+            toggleReadProperty(i);
+            displayBooks();
+        });
+        card.append(bookIcon);
+        
         //adds card to main element
         main.appendChild(card);
         
@@ -103,11 +109,27 @@ function addPropertyToCard(property, preface) {
     return para;
 }
 
+//return a input element of type image with an src and alt
+function createImageElement(src, alt) {
+    let image = document.createElement('input');
+    image.setAttribute('type', 'image');
+    image.setAttribute('src', src);
+    image.setAttribute('alt', alt);
+    return image;
+}
+
+//removes an element at index from myLibrary array
 function removeCardFromLibrary(index) {
     myLibary.splice(index, 1);
 }
 
-addBookToLibrary(new Book("Song of Ice and Fire", "Author1", 123, true));
-addBookToLibrary(new Book("I am Legend", "Author2", 321, false));
-addBookToLibrary(new Book("American Psycho", "Author3", 666, true));
+//toggles read property in book at index in myLibrary array
+function toggleReadProperty(index) {
+    myLibary[index].read === 'Yes' ? myLibary[index].read = 'No' : myLibary[index].read = 'Yes';
+}
+
+addBookToLibrary(new Book("Song of Ice and Fire", "Author1", 123, 'Yes'));
+addBookToLibrary(new Book("I am Legend", "Author2", 321, 'No'));
+addBookToLibrary(new Book("American Psycho", "Author3", 666, 'No'));
 displayBooks();
+
